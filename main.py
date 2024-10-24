be5555b95711cdb062002f6af4715f77e251d296
rafikee <rafik.vcu@gmail.com>
2024-10-24 09:38:09 -0700

    added try block for mqtt

--- main.py
+++ main.py
@@ -19,15 +19,26 @@ def main():
     wifi_success = startup()
     if wifi_success:
         gc.collect()
-        mqtt_run()
+        try:
+            mqtt_run()
+        except Exception as e:
+            print("MQTT failed, retrying:", e)
+            return
     else:
         run_ble()
 
+
+error_count = 0
+max_errors = 3
 
 while True:
     try:
         main()
     except Exception as e:
-        print("An error occured", e)
+        print("An error occurred", e)
         led_off()
+        error_count += 1
+        if error_count > max_errors:
+            run_ble()  # Fall back to BLE instead of resetting again
+            break  # Exit the reset loop
         machine.reset()
